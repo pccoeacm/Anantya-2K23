3970b985fa18a3d0ed1d0d9288d285c041d2cea2
surajTade <suraj.tade20@pccoepune.org>
2023-03-04 15:42:00 +0530

    Structed the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Events from './pages/Events'
-import Calender from './pages/Calender'
+import "./styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import Calender from "./pages/Calender";
+import React from "react";
 
 function App() {
-  return (
-    <>
-     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/events" element={<Events/>} />
-          <Route path="/calender" element={<Calender/>} />
-        </Routes>
-     </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/events" element={<Events />} />
+					<Route path="/calender" element={<Calender />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,56 @@
-import React from 'react'
-import './Navbar.css'
-import { NavLink} from "react-router-dom"
+import React from "react";
+import "../styles/Navbar.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <>
-        <nav className="navbar navbar-expand-lg ">
-  <div className="container-fluid">
-  <NavLink to="/" className='"navbar-brand title'>Anantya</NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      {/* <span className="navbar-toggler-icon"></span> */}
-      <img src="menu.png" alt="" className='menu-img' />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav  ms-auto">
-      <li className="nav-item">
-        <NavLink to="/" className='nav-link'><span className='navitem'>Home</span></NavLink>  
-        </li>
-        <li className="nav-item">
-        <NavLink to="/about" className='nav-link'><span className='navitem'>About</span></NavLink> 
-        </li>
-        <li className="nav-item">
-        <NavLink to="/events" className='nav-link'><span className='navitem'>Events</span></NavLink>
-        </li>
-        <li className="nav-item">
-        <NavLink to="/calender" className='nav-link'><span className='navitem'>Calender</span></NavLink>
-          {/* <a className="nav-link" href="#"></a> */}
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </>
-  )
-}
+	return (
+		<>
+			<nav className="navbar navbar-expand-lg ">
+				<div className="container-fluid">
+					<NavLink to="/" className='"navbar-brand title'>
+						Anantya
+					</NavLink>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						{/* <span className="navbar-toggler-icon"></span> */}
+						<img src="menu.png" alt="" className="menu-img" />
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav  ms-auto">
+							<li className="nav-item">
+								<NavLink to="/" className="nav-link">
+									<span className="navitem">Home</span>
+								</NavLink>
+							</li>
+							<li className="nav-item">
+								<NavLink to="/about" className="nav-link">
+									<span className="navitem">About</span>
+								</NavLink>
+							</li>
+							<li className="nav-item">
+								<NavLink to="/events" className="nav-link">
+									<span className="navitem">Events</span>
+								</NavLink>
+							</li>
+							<li className="nav-item">
+								<NavLink to="/calender" className="nav-link">
+									<span className="navitem">Calender</span>
+								</NavLink>
+								{/* <a className="nav-link" href="#"></a> */}
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</>
+	);
+};
 
-export default Navbar
+export default Navbar;
