5068b3a9601be35834a60f63e0738cf88eb126e2
surajTade <suraj.tade20@pccoepune.org>
2023-03-04 16:16:38 +0530

    added assets folder, started home page work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import "../styles/Home.css";
+import { astronaut } from "../assets";
 
 const Home = () => {
 	return (
 		<>
 			<Navbar />
-			<div style={{ textAlign: "center" }}>
-				<h1 style={{ color: "white" }}>Home</h1>
-			</div>
-			<Footer />
+			<div className="bg-1-gradient">.</div>
+			<div className="bg-2-gradient">.</div>
+			<img src={astronaut} className="astronaut" />
+			<div className="bg-3-gradient">.</div>
+
+			{/* <Footer /> */}
 		</>
 	);
 };
