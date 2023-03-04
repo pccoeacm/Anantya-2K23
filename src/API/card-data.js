1cb4969587ee083b548669ac531adf8af433ee98
sarthak-subhash-joshi <sarthakjoshi3011@gmail.com>
2023-03-04 16:17:24 +0530

    fixing footer

---
+++ src/API/card-data.js
@@ -0,0 +1,57 @@
+const cardData = [
+    {
+      id: 1,
+      image:"https://tse4.mm.bing.net/th?id=OIP.fjpz0GPcZ53la1oEi_q5IgHaE7&pid=Api&P=0",
+      category: "ring cermony"
+    },
+  
+    {
+      id: 2,
+      image:"https://tse3.explicit.bing.net/th?id=OIP.vFpEpCrFcG5O8Miu0-PL6gHaE7&pid=Api&P=0",
+      category: "ring cermony"
+    },
+    {
+      id: 3,
+      image:"https://tse2.mm.bing.net/th?id=OIP.Hubz7bEACP1ZVT2tTlvs1QHaE8&pid=Api&P=0",
+      category: "pre wedding"
+    },
+    {
+        id: 4,
+        image:"https://tse2.mm.bing.net/th?id=OIP.Kiv9oizwQ8lsDZj9MWq0dQHaE8&pid=Api&P=0",
+        category: "pre wedding"
+    },
+    {
+      id: 5,
+      image:"https://tse2.mm.bing.net/th?id=OIP.ibNeSkSET3NNZV1i9LuCDwHaE8&pid=Api&P=0",
+      category: "pre wedding"
+    },
+    {
+        id: 6,
+        image:"https://tse1.mm.bing.net/th?id=OIP.mHUvovZxG8HKJKZPF2vUXQHaHa&pid=Api&P=0",
+        category: "wedding"
+      },
+    {
+      id: 7,
+      image:"https://tse3.mm.bing.net/th?id=OIP.VUCC75VDj_8kZgmIvF_8xwHaLG&pid=Api&P=0",
+      category: "wedding"
+    },
+    {
+      id: 8,
+      image:
+        "https://tse1.mm.bing.net/th?id=OIP.4xbQHHZPgawVSKW4nVvoNgHaLH&pid=Api&P=0",
+      category: "wedding"
+    },
+    {
+        id: 9,
+        image:"https://tse1.mm.bing.net/th?id=OIP.AopaoCuCI0AxIFgkW1AduAHaLH&pid=Api&P=0",
+        category: "wedding"
+      },
+      {
+        id: 10,
+        image:"https://tse3.mm.bing.net/th?id=OIP.-zUPcQuRaEWjpGyNNdXESQHaLH&pid=Api&P=0",
+        category: "bridal shower"
+      }
+  ];
+  
+  export default cardData;
+  
