92a2c3d905433957d5f7ce059e47a0116e743a5e
atharvapande45 <atharva.pande20@pccoepune.org>
2023-03-06 23:40:06 +0530

    Added particles background

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Card from '../components/Card'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+import Design from "../components/Design";
 
 const Events = () => {
   return (
     <>
-      <Navbar/>
-      
+      <Design />
+      <Navbar />
+
       {/* <div style={{textAlign:'center',marginTop:'70px'}}>
       <h1 style={{color:'white', fontSize: '6rem', fontWeight: '400' }} >EVENTS</h1>
       </div> */}
 
       <Card />
     </>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
