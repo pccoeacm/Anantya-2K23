c8aa213a10b64eea82605aff7f62152ee3588124
surajTade <suraj.tade20@pccoepune.org>
2023-03-15 21:09:02 +0530

    removed unused code

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,83 +6,79 @@ import Design from "../components/Design";
 import { NavLink } from "react-router-dom";
 
 const About = ({ ids }) => {
-  return (
-    <>
-      {/* <Design /> */}
-      <Navbar />
+	return (
+		<>
+			<Navbar />
 
-      {Data.filter((item) => item.id == ids).map((elem) =>{
-        return(
-          <>
-          <div
-        className="row"
-        style={{ margin: "5% 3%", display: "flex", justifyContent: "center" }}
-      >
-        <div className="bg-1-gradient"></div>
-        <div className="card col-lg-4">
-          <div style={{ textAlign: "center" }}>
-            <img
-              src="https://t4.ftcdn.net/jpg/05/59/08/01/240_F_559080129_4fWvQkVrQQCAJxaLFstZIfmWUW7DEgQ2.jpg"
-              alt="image"
-            />
-          </div>
-        </div>
-        <div className="col-lg-8 events-details-container">
-          <div style={{ textAlign: "center" }}>
-            <h1>{elem.event_name}</h1>
-          </div>
+			{Data.filter((item) => item.id == ids).map((elem) => {
+				return (
+					<>
+						<div
+							className="row"
+							style={{
+								margin: "5% 3%",
+								display: "flex",
+								justifyContent: "center",
+							}}>
+							<div className="bg-1-gradient"></div>
+							<div className="card col-lg-4">
+								<div style={{ textAlign: "center" }}>
+									<img
+										src="https://t4.ftcdn.net/jpg/05/59/08/01/240_F_559080129_4fWvQkVrQQCAJxaLFstZIfmWUW7DEgQ2.jpg"
+										alt="image"
+									/>
+								</div>
+							</div>
+							<div className="col-lg-8 events-details-container">
+								<div style={{ textAlign: "center" }}>
+									<h1>{elem.event_name}</h1>
+								</div>
 
-          <div style={{ margin: "5%" }}>
-            <h2>Description</h2>
-          </div>
+								<div style={{ margin: "5%" }}>
+									<h2>Description</h2>
+								</div>
 
-          <div className="event-details-description">
-            <p className="description-para-tag">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-              libero non laborum repudiandae? Dolorum aspernatur voluptatibus
-              accusantium officiis numquam, asperiores consectetur temporibus,
-              neque voluptate mollitia voluptates delectus dolores magni magnam.
-            </p>
-          </div>
+								<div className="event-details-description">
+									<p className="description-para-tag">
+										Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+										Aliquam libero non laborum repudiandae? Dolorum aspernatur
+										voluptatibus accusantium officiis numquam, asperiores
+										consectetur temporibus, neque voluptate mollitia voluptates
+										delectus dolores magni magnam.
+									</p>
+								</div>
 
-          <div className="event-details-description">
-            <div className="row description-para-tag">
-              <div className="col-lg-4">
-                <h2>Type</h2>
-                <p>Team / Solo</p>
-              </div>
-              <div className="col-lg-4">
-                <h2>Prizes</h2>
-                <p>Rank 1: 1000</p>
-                <p>Rank 2: 500</p>
-                <p>Rank 3: 300</p>
-              </div>
-              <div className="col-lg-4">
-                <h2>Fees</h2>
-                <p>Free</p>
-              </div>
-            </div>
-          </div>
+								<div className="event-details-description">
+									<div className="row description-para-tag">
+										<div className="col-lg-4">
+											<h2>Type</h2>
+											<p>Team / Solo</p>
+										</div>
+										<div className="col-lg-4">
+											<h2>Prizes</h2>
+											<p>Rank 1: 1000</p>
+											<p>Rank 2: 500</p>
+											<p>Rank 3: 300</p>
+										</div>
+										<div className="col-lg-4">
+											<h2>Fees</h2>
+											<p>Free</p>
+										</div>
+									</div>
+								</div>
 
-
-          
-          <NavLink to={elem.path} className="nav-link">
-									<span className="navitem"><button>Register</button></span>
-					</NavLink>
-
-
-        </div>
-      </div>
-          </>
-        );
-      })}
-        
-    
-    
-      
-     
-    </>
-  );
+								<NavLink to={elem.path} className="nav-link">
+									<span className="navitem">
+										<button>Register</button>
+									</span>
+								</NavLink>
+							</div>
+						</div>
+					</>
+				);
+			})}
+		</>
+	);
 };
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,11 @@
 import React from "react";
 import { HomeCss } from "../styles";
-import { Design, Footer, ISS, Navbar, Sponsorcard, About } from "../components";
-import { astronaut, aboutBG, aboutBGM } from "../assets";
+import { Navbar, Sponsorcard, About } from "../components";
 
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
-			{/* <ISS /> */}
-			{/* <div className="container about-section">
-				<div className="grid-main">
-					<div className="about-bg">
-						<img src={aboutBG} className="img-fluid about-bg-img" />
-						<img src={aboutBGM} className="img-fluid about-bgm-img" />
-					</div>
-					<div className="heading-cont">
-						<div className="about-heading">
-							<h1 className="about-anantya">ANANTYA</h1>
-						</div>
-						<div className="about-desc-div">
-							<p className="about-desc-content">
-								Contrary to popular belief, Lorem Ipsum is not simply random
-								text. It has roots in a piece of classical Latin literature from
-								45 BC, making it over 2000 years old. Richard McClintock, a
-								Latin professor at Hampden-Sydney College in Virginia, looked up
-								one of the more obscure Latin words, consectetur, from a Lorem
-								Ipsum passage, and going through the cites of the word in
-								classical literature, discovered the undoubtable source. Lorem
-								Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-								Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-								written in 45 BC. This book is a treatise on the theory of
-								ethics, very popular during the Renaissance. The first line of
-								Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
-								in section 1.10.32.
-							</p>
-						</div>
-			{/* <ISS /> */}
 			<About />
 			<div className="container sponsors-main-div">
 				<div className="row">
