ea976b6af3290d25fafa7e578903e534d66cba65
surajTade <suraj.tade20@pccoepune.org>
2023-03-18 21:49:36 +0530

    minor changes in card-data, design and about section, improved responsiveness of all components

--- src/components/HAbout.js
+++ src/components/HAbout.js
@@ -4,9 +4,7 @@ import ISS from "./ISS";
 const HAbout = () => {
 	return (
 		<div className="container about-main-div">
-			<div className="iss-bg">
-				<ISS />
-			</div>
+			<div className="iss-bg">{/* <ISS /> */}</div>
 			<div className="row rounded-4 d-flex justify-content-between">
 				<h1 className="about-heading rounded-4">ANANTYA</h1>
 			</div>
